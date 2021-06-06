4573651d97e8b0cca6dab018cc2c7bc95a9646da
7204-OMER <Eyupbhr24@gmail.com>
2021-06-07 00:45:45 +0300

    add index.css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import "./App.css";
+import { useState } from "react";
 import Form from "./components/form/Form";
 import Contact from "./components/contacts/Contact";
 
+const initialValues = { username: "", phoneNumber: "", gender: "NO INFO!" };
+
 function App() {
+  const [info, setInfo] = useState(initialValues);
   return (
     <div className="App">
-      <h2>Test</h2>
-      <Form />
+      <Form className="form" info={info} setInfo={setInfo} />
       <Contact />
     </div>
   );
