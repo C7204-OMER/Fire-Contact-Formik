1faddb131a6c832f96a9f21f3eda8aa9c0d81e74
7204-OMER <Eyupbhr24@gmail.com>
2021-06-07 00:38:30 +0300

    add components scheleton

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# Fire-Contact-Formik
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Form from "./components/form/Form";
+import Contact from "./components/contacts/Contact";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Test</h2>
+      <Form />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
